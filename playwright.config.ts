e6055ae3964d777dcc719238d1ab1454573a43fe
Lucija Kranjcec <lucija.kranjcec@rise-world.com>
2024-03-04 17:39:33 +0100

    innitial project setup

---
+++ playwright.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 10000
+  },
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: 3,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'localhost:8080',
+    headless: false,
+    trace: 'on',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    }, 
+  ],
+
+  outputDir: 'test-results/',
+});
